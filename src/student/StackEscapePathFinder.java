a9c86b8bf52cfb6102b7de29a5486fe41d8bc23a
simon <saird02@mail.bbl>
2016-01-17 18:07:49 +0000

    Staged implementation of stack path finder

--- src/student/StackEscapePathFinder.java
+++ src/student/StackEscapePathFinder.java
@@ -1,12 +1,16 @@
 package student;
 
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
 import game.EscapeState;
 import game.Node;
 
 /**
  * Path finder which uses the shortest path as a base. New paths are created by
  * descending the path from the exit looking for alternative routes which start
- * close to the exit and get progressively longer.
+ * close to the exit and get progressively longer. The shortest escape route
+ * will be returned as a default.
  */
 public class StackEscapePathFinder extends AbstractEscapePathFinder {
 
@@ -14,6 +18,8 @@ public class StackEscapePathFinder extends AbstractEscapePathFinder {
 	private Node exitCovering;
 
 	private long timeout; // Elapsed time of exit planning
+	
+	private Deque<EscapePath>  stack;
 
 	public StackEscapePathFinder(EscapeState state) {
 		super(state);
@@ -30,6 +36,11 @@ public class StackEscapePathFinder extends AbstractEscapePathFinder {
 
 		// Set the shortest escape route as a default
 		escapePath = new ShortestEscapePathFinder(state).findEscapePath(escapeState);
+		
+		//
+		stack = new ConcurrentLinkedDeque<>();
+		
+		
 		System.out.println(String.format("%d additional paths found", numberOfPathsFound));
 		return escapePath;
 	}
