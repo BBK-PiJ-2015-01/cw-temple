14064c5ef501634259ff3c93b6e792f52725646d
sbaird02 <sbaird02@mail.bbk.ac.uk>
2016-01-17 13:51:27 +0000

    Created shortest escape path finder

---
+++ src/student/ShortestEscapePathFinder.java
@@ -0,0 +1,13 @@
+package student;
+
+import game.EscapeState;
+
+public class ShortestEscapePathFinder implements EscapePathFinder {
+
+	@Override
+	public EscapePath findEscapePath(EscapeState state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
