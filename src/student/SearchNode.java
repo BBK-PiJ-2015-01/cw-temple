f4cddfc3eb58d0e95ece5c1f1627ad1c49aed695
simon <saird02@mail.bbl>
2016-01-17 10:37:51 +0000

    Added SearchNode

---
+++ src/student/SearchNode.java
@@ -0,0 +1,53 @@
+package student;
+
+import game.Node;
+
+public class SearchNode {
+
+	private Node node;
+	private Node parentNode;
+	private int gCost;
+	private int hCost;
+
+	
+	public SearchNode(Node node, Node parentNode, int gCost, int hCost) {
+
+		this.node = node;
+		this.parentNode = parentNode;
+		this.gCost = gCost;
+		this.hCost = hCost;
+	}
+
+	public Node getNode() {
+		return node;
+	}
+
+	public void setNode(Node node) {
+		this.node = node;
+	}
+
+	public Node getParentNode() {
+		return parentNode;
+	}
+
+	public void setParentNode(Node parentNode) {
+		this.parentNode = parentNode;
+	}
+
+	public int getgCost() {
+		return gCost;
+	}
+
+	public void setgCost(int gCost) {
+		this.gCost = gCost;
+	}
+
+	public int gethCost() {
+		return hCost;
+	}
+
+	public void sethCost(int hCost) {
+		this.hCost = hCost;
+	}
+
+}
